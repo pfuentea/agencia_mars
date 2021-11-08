184b5304e348cfcb26ee7fa2e9aafb81f00c3714
Patricio Fuentealba <patricio.fuentealba.feliu@gmail.com>
2021-11-08 12:59:43 -0300

    Regiones - Provincias - Cmunas

--- test_iat/models.py
+++ test_iat/models.py
@@ -107,17 +107,26 @@ class Region(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+    def __str__ (self):
+        return f"{self.ordinal} : {self.nombre}"
+
 class Provincia(models.Model):
     nombre = models.CharField(max_length=100)
     region = models.ForeignKey(Region, related_name="provincias", on_delete = models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+    def __str__ (self):
+        return self.nombre
+
 class Comuna(models.Model):
     nombre = models.CharField(max_length=100)
     provincia = models.ForeignKey(Provincia, related_name="comunas", on_delete = models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__ (self):
+        return self.nombre
 
 class User(models.Model):
     CHOICES = (
