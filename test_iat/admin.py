8b1553a57666522954816868ef5a6ddcfb2fce78
Patricio Fuentealba <patricio.fuentealba.feliu@gmail.com>
2021-11-21 23:22:09 -0300

    Agrega Descargas al admin

--- test_iat/admin.py
+++ test_iat/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import User, Comuna,Region,Provincia,Sondeo
+from .models import User, Comuna,Region,Provincia,Sondeo,Descargas
 
 # Register your models here.
 admin.site.register(User)
@@ -7,4 +7,5 @@ admin.site.register(Comuna)
 admin.site.register(Region)
 admin.site.register(Provincia)
 admin.site.register(Sondeo)
+admin.site.register(Descargas)
 
