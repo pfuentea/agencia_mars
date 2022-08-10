5d6a51672e8af776a7fb7f8d7941754db5ccd4e7
Patricio Fuentealba <pfuenteabl.deloitte@bancodechile.cl>
2022-08-10 14:17:58 -0400

    no le gusto el tilde 2

--- test_iat/models.py
+++ test_iat/models.py
@@ -181,7 +181,7 @@ class User(models.Model):
 class Combinacion(models.Model):
     test = models.ForeignKey(Test, related_name="combinaciones", on_delete = models.CASCADE)
     participante=models.ForeignKey(User, related_name="combinaciones", on_delete = models.CASCADE)
-    indice=models.IntegerField() # número de generación al iniciar el test
+    indice=models.IntegerField() # numero de generacion al iniciar el test
     analisis=models.IntegerField() # del 1 al 4
     valor=models.CharField(max_length=500)     
     created_at = models.DateTimeField(auto_now_add=True)
