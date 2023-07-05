6c7190230279e4f91252c8cdb26097b2b5d2c12c
Patricio Fuentealba <pfuenteabl.deloitte@bancodechile.cl>
2023-07-04 20:38:16 -0400

    ADD: Nuevos modelos al admin

--- test_iat/admin.py
+++ test_iat/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import User, Comuna,Region,Provincia,Sondeo,Descargas
+from .models import User, Comuna,Region,Provincia,Sondeo,Descargas,Categoria,Cliente,Test,Caracteristica,Adjetivo,Producto,Tcategoria,Tcaracteristicas,Tadjetivos,Tproductos,Tatributos,Combinacion,Resultado
 
 # Register your models here.
 admin.site.register(User)
@@ -8,4 +8,23 @@ admin.site.register(Region)
 admin.site.register(Provincia)
 admin.site.register(Sondeo)
 admin.site.register(Descargas)
+
+admin.site.register(Categoria)
+
+admin.site.register(Cliente)
+admin.site.register(Test)
+admin.site.register(Caracteristica)
+admin.site.register(Adjetivo)
+admin.site.register(Producto)
+
+admin.site.register(Tcategoria)
+admin.site.register(Tcaracteristicas)
+admin.site.register(Tadjetivos)
+admin.site.register(Tproductos)
+admin.site.register(Tatributos)
+
+admin.site.register(Combinacion)
+admin.site.register(Resultado)
+
+
 
